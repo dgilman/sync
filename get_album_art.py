dadb3e9989b49c78cd98859c0bc3129b00c4f40b
David Gilman <davidgilman1@gmail.com>
2017-09-12 22:56:44 -0400

    Clean up comments in python

--- get_album_art.py
+++ get_album_art.py
@@ -38,7 +38,6 @@ class Updater(object):
             self.reset_req_count()
 
         if self.api_req_remaining == 0:
-            print('sleeping')
             time.sleep((self.api_req_reset - now).total_seconds()+1)
             self.bump_req_time()
 
@@ -51,7 +50,6 @@ class Updater(object):
         self.c.execute("SELECT image FROM cache WHERE artist = ? AND release_title = ?", (artist, release_title))
         cache = self.c.fetchall()
         if len(cache) != 0:
-            print('cache hit')
             return cache[0][0]
 
         search_resp = self._request(self.URL_BASE + '/database/search', {"artist": artist, "release_title": release_title})
@@ -64,7 +62,7 @@ class Updater(object):
             release_id = releases[0]['id']
             release_resp = self._request(self.URL_BASE + '/releases/{0}'.format(release_id))
         else:
-            print('no search results found')
+            print('No search results found for {0}: {1}'.format(artist, release_title))
             return None
 
         primary_images = [x for x in release_resp['images'] if x['type'] == 'primary']
@@ -74,7 +72,6 @@ class Updater(object):
         elif len(secondary_images) > 0:
             image_url = secondary_images[0]['uri']
         else:
-            print('no primary images found')
             return None
 
         image = requests.get(image_url).content
@@ -86,7 +83,6 @@ class Updater(object):
         mp3 = mutagen.mp3.MP3(mp3_path.as_posix())
 
         if len(mp3.tags.getall('APIC')) > 0:
-            print('skipping', mp3_path.as_posix())
             return
 
         talbs = mp3.tags.getall('TALB')
@@ -99,14 +95,9 @@ class Updater(object):
             return
         artist = tpe1s[0].text[0]
 
-        print(artist, release_title)
         artwork = self.get_artwork(artist, release_title)
         if artwork == None:
-            print('no artwork found!')
             return
-        #apic = mutagen.id3.APIC(mime='foo', type=3 (or maybe 0), data='bytes')
-        #mp3.tags.add(apic)
-        #mp3.save()
         apic = mutagen.id3.APIC(mime='image/jpeg', type=3, data=artwork)
         mp3.tags.add(apic)
         mp3.save()
